e3b67494ac2a8b4ac227b9e59e73a8e165d2b0e3
mohamad <msiysinyuy@gmail.com>
2021-08-29 23:01:25 +0100

    the product page is returning and displaying products with images

--- frontend/src/components/products/Products.jsx
+++ frontend/src/components/products/Products.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import {Grid} from "@material-ui/core"
 import Product from './product/Product'
+import img1 from '../../images/MacBook-Pro.jpg'
+import img2 from '../../images/running-shoes.jpeg'
 
 const products=[
-  {id:1,name:"Shoes",description:"Running Shoes",price:"$6"},
-  {id:2,name:"Macbook",description:"Apple Mackbook",price:"$8"},
-  {id:3,name:"Shoes3",description:"Running Shoes",price:"$5"},
-  {id:4,name:"Shoes4",description:"Running Shoes",price:"$1"},
-  {id:5,name:"Shoes5",description:"Running Shoes",price:"$67"},
+  {id:1,name:"Shoes",description:"Running Shoes",price:"$6",image:img2},
+  {id:2,name:"Macbook",description:"Apple Mackbook",price:"$8",image:img1},
+  {id:3,name:"Shoes3",description:"Running Shoes",price:"$5",image:img2},
+  {id:4,name:"Shoes4",description:"Running Shoes",price:"$1",image:img2},
+  {id:5,name:"Shoes5",description:"Running Shoes",price:"$67",image:img2},
 ]
 
 const Products=()=>{
