bc909d8b3279b162b75991d03599ac0c23c87034
mohamad <msiysinyuy@gmail.com>
2021-08-30 11:34:25 +0100

    the navbar is done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import "./styles/App.css"
-import Products from "./components/products/Products"
+
+import {Products, Navbar} from './components'
 function App(){
   return(
       <div>
+          <Navbar />
           <Products/>
       </div>
 
