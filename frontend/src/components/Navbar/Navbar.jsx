359193ae668f58c4c9bd5513210086db2853f8fc
mohamad <msiysinyuy@gmail.com>
2021-08-30 13:53:27 +0100

    products are now fetch form the commerse api and clicking on items add them to cart

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import {AppBar,Toolbar,IconButton,Badge,MenuItem,Menu,Typography,} from '@materi
 import {ShoppingCart} from '@material-ui/icons'
 import useStyles from './styles'
 import logo from '../../assets/logo.png'
-const Navbar =()=>{
+const Navbar =({totalItems})=>{
   const classes=useStyles()
   return(
     <>
@@ -15,7 +15,7 @@ const Navbar =()=>{
                 <div className={classes.grow}/>
                 <div className={classes.button}>
                   <IconButton aria-label="show cart items" color="inherit">
-                      <Badge badgeContent={2} color="secondary">
+                      <Badge badgeContent={totalItems} color="secondary">
                           <ShoppingCart/>
                       </Badge>
                   </IconButton>
